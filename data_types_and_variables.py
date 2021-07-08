e529a5f20d6cb943d6321773689bee14a42371a5
chriseverts <chriseverts21@gmail.com>
2021-07-08 17:01:16 -0500

    'initial commit'

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -1,9 +1,65 @@
-vars littlemermaid = 3*3:
 
-vars brotherbear = 5*3:
+##1 You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it). If price for a movie per day is 3 dollars, how much will you have to pay?
 
-vars  Hercules = 1*3:
+vars littlemermaid = 3*3; ## variables
 
-priceofmovies = littlemermaid + brotherbear + Hercules
+vars brotherbear = 5*3;
+
+vars  Hercules = 1*3;
+
+priceofmovies (a,b,c) 
+return littlemermaid + brotherbear + Hercules; ## addition of variables.
+
+##2 Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays 400 dollars per hour, Amazon 380, and Facebook 350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+
+google = 400 * 6
+
+amazon = 380 * 4
+
+facebook = 350 * 10
+
+def company(a,b,c,):   
+    return (google + amazon + facebook)
+
+
+##A student can be enrolled to a class only if the class is not full and the class schedule does not conflict with her current schedule. 
+
+If student x > 20 and x == 2 = true
+
+
+
+
+## A product offer can be applied only if people buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.
+
+if x > 2 and not expired then n
+
+
+## Create a variable that holds a boolean value for each of the following conditions:
+
+##the password must be at least 5 characters
+##the username must be no more than 20 characters
+##the password must not be the same as the username
+##bonus neither the username or password can start or end with whitespace
+
+def getPassword(password):
+    if len(password) < 5:
+        print('the password must be at least 5 characters')
+        val = False
+
+def getuser(username):
+    if len(username) > 20:
+        print('the username must be no more than 20 characters')
+        val = False
+
+def user(samename):
+    x = password
+    if samename = password 
+        print('the password must not be the same as the username')
+        val = False
+
+        
+
+
+
 
 
