c2a4117b384afa264f21b64d2771ed871f245da7
chriseverts <chriseverts21@gmail.com>
2021-07-09 14:53:16 -0500

    initial commit

---
+++ control_structures_excercises.py
@@ -0,0 +1,32 @@
+## 1. Conditional Basics
+
+####prompt the user for a day of the week, print out whether the day is Monday or not
+
+##prompt the user for a day of the week, print out whether the day is a weekday or a weekend
+
+##create variables and make up values for
+
+####the number of hours worked in one week
+##the hourly rate
+##how much the week's paycheck will be
+##write the python code that calculates the weekly paycheck. You get paid time and a half if you work more than 40 hours
+
+
+day_of_week = 'Monday'
+
+If day_of_week == 'Monday':
+    print('Yes')
+elif(day_of_week == 'Tuesday':
+    print('No')
+elif(day_of_week == 'Wensday':
+    print('No')
+elif(day_of_week == 'Thursday':
+    print('No')
+elif(day_of_week == 'Friday':
+    print('No')
+elif(day_of_week == 'Saturday':
+    print('No')
+elif(day_of_week == 'Sunday':
+    print('No')
+else:
+    print('Its not Monday')
