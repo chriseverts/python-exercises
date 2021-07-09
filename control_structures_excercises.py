21b3b99edce6b03e66e040595bd64f208862e5b9
chriseverts <chriseverts21@gmail.com>
2021-07-09 16:44:13 -0500

    more answers

--- control_structures_excercises.py
+++ control_structures_excercises.py
@@ -98,3 +98,8 @@ for i in range(1, 11):
         print("{x} x {i} = {x * i}")
 
 
+##Create a for loop that uses print to create the output shown below.
+
+for i in range(10):
+    print(str(i) * i)
+    
