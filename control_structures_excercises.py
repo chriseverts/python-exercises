9208334e6d5db09ed9835dc178926599dd90aa04
chriseverts <chriseverts21@gmail.com>
2021-07-09 15:36:03 -0500

    added answers

--- control_structures_excercises.py
+++ control_structures_excercises.py
@@ -4,9 +4,7 @@
 
 ##prompt the user for a day of the week, print out whether the day is a weekday or a weekend
 
-##create variables and make up values for
-
-####the number of hours worked in one week
+##create variables and make up values for the number of hours worked in one week
 ##the hourly rate
 ##how much the week's paycheck will be
 ##write the python code that calculates the weekly paycheck. You get paid time and a half if you work more than 40 hours
@@ -29,4 +27,43 @@ elif(day_of_week == 'Saturday':
 elif(day_of_week == 'Sunday':
     print('No')
 else:
-    print('Its not Monday')
+
+
+
+##2 Loop Basics
+
+##While
+
+##Create an integer variable i with a value of 5.
+##Create a while loop that runs so long as i is less than or equal to 15
+##Each loop iteration, output the current value of i, then increment i by one.
+
+
+
+
+i = 5
+while i <= 15:
+    print(i)
+    i += 1
+
+i = 0
+while i <= 100:
+    print(i)
+    i += 2
+
+i = 100
+while i >= -10:
+    print(i)
+    i -= 5
+
+i = 2
+while i <= 1_000_000:
+    print(i)
+    i = i ** 2
+
+x = (input("Please enter a number:"))
+
+for i in range(1, 11):  
+        print("{x} x {i} = {x * i}")
+
+
