67e98afe7f367989f0b88340321a95cb16c9f4cc
chriseverts <chriseverts21@gmail.com>
2021-07-13 10:40:04 -0500

    more answers

--- function_exercises.py
+++ function_exercises.py
@@ -36,10 +36,76 @@ print(user.title())
 
 
 ##5. Define a function named calculate_tip. It should accept a tip percentage (a number between 0 and 1) and the bill total, and return the amount to tip.
-def calculateTip(total_cost,tip):
+def calculate_tip(total_cost,tip):
         total_cost = 20
         tip = 1  
         new_total = (total_cost * tip) / 100 + total_cost
         return new_total
 
         
+##6. Define a function named apply_discount. 
+# It should accept a original price, and a discount percentage,
+#  and return the price after the discount is applied.
+
+
+def apply_discount(a,b):
+    discount = b - a
+    dispercent = (discount/b) *100
+    total_price = (b * dispercent) - b
+    return total_price
+
+##7Define a function named handle_commas. 
+# It should accept a string that is a number that contains commas in it as input, 
+# and return a number as output.
+
+def handle_commas(b):
+    return float(b.replace(",", ""))
+
+
+##8. Define a function named get_letter_grade. 
+# It should accept a number and return the letter grade associated with that number (A-F).
+
+def get_letter_grade(grade):
+    if grade >=90:
+        return "A"
+    elif (grade <=89) and (grade >=80):
+        return "B"
+    elif (grade <=79) and (grade>=70):
+        return "C"
+    elif (grade >=60) and (grade <=69):
+        return "D"
+    else:
+        return “F”
+        
+##9. Define a function named remove_vowels that accepts a string 
+# and returns a string with all the vowels removed.
+
+
+string = input("Enter any string: ")
+def remove_vowels(c):
+    newstr = c
+    vowels = ('a', 'e', 'i', 'o', 'u')
+    for x in c.lower():
+        if x in vowels:
+            newstr = newstr.replace(x,"")
+
+    return newstr
+remove_vowels(string)
+
+
+##10 Define a function named normalize_name. 
+# It should accept a string and return a valid python identifier, that is:
+
+
+
+
+##11.Write a function named cumulative_sum that accepts a list of numbers and returns a list that is the cumulative sum of the numbers in the list.
+##cumulative_sum([1, 1, 1]) returns [1, 2, 3]
+##cumulative_sum([1, 2, 3, 4]) returns [1, 3, 6, 10]
+
+def cumulative_sum(lists):
+    cu_list = []
+    length = len(lists)
+    cu_list = sum(lists[0:x:1]) 
+    for x in range(0, length+1):
+            return cu_list[1:]
