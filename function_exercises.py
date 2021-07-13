0d1e2095863befc5bccb1f3a74bbed1c6d2f8538
chriseverts <chriseverts21@gmail.com>
2021-07-13 09:42:13 -0500

    more answers

--- function_exercises.py
+++ function_exercises.py
@@ -27,6 +27,19 @@ def is_consonant(string):
 
 
 
+##4. Define a function that accepts a string that is a word. The function should capitalize the first letter of the word if the word starts with a consonant.
 
+user = input("Please enter a sentance")
+
+print(user.title())
+
+
+
+##5. Define a function named calculate_tip. It should accept a tip percentage (a number between 0 and 1) and the bill total, and return the amount to tip.
+def calculateTip(total_cost,tip):
+        total_cost = 20
+        tip = 1  
+        new_total = (total_cost * tip) / 100 + total_cost
+        return new_total
 
         
